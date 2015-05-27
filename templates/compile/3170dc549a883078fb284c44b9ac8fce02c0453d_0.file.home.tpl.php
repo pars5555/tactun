3e5585362df2d19a6d1b9cfce6816b5049586c06
pars5555 <pars5555@yahoo.com>
2015-05-27 20:14:17 +0400

    1234

--- templates/compile/3170dc549a883078fb284c44b9ac8fce02c0453d_0.file.home.tpl.php
+++ templates/compile/3170dc549a883078fb284c44b9ac8fce02c0453d_0.file.home.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.23, created on 2015-05-26 18:11:50
+<?php /* Smarty version 3.1.23, created on 2015-05-27 18:14:00
          compiled from "D:/xampp/htdocs/tactun/templates//main/home.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:2669755649b46053149_05848556%%*/
+/*%%SmartyHeaderCode:23625565ed485988b3_15428311%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,30 +9,25 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '3170dc549a883078fb284c44b9ac8fce02c0453d' => 
     array (
       0 => 'D:/xampp/htdocs/tactun/templates//main/home.tpl',
-      1 => 1432656501,
+      1 => 1432743235,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '2669755649b46053149_05848556',
-  'variables' => 
-  array (
-    'ns' => 0,
-  ),
+  'nocache_hash' => '23625565ed485988b3_15428311',
   'has_nocache_code' => false,
   'version' => '3.1.23',
-  'unifunc' => 'content_55649b46057836_27184560',
+  'unifunc' => 'content_5565ed485bd077_70911608',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_55649b46057836_27184560')) {
-function content_55649b46057836_27184560 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_5565ed485bd077_70911608')) {
+function content_5565ed485bd077_70911608 ($_smarty_tpl) {
 ?>
 <?php
-$_smarty_tpl->properties['nocache_hash'] = '2669755649b46053149_05848556';
+$_smarty_tpl->properties['nocache_hash'] = '23625565ed485988b3_15428311';
 ?>
 <div class="welcome">
   <h1>nested home load</h1>
-  <h2>Hello <?php echo $_smarty_tpl->tpl_vars['ns']->value['demoDto']->getName();?>
-</h2>
+  <h2>Hello</h2>
 </div>
 <?php }
 }
--- templates/main/home.tpl
+++ templates/main/home.tpl
@@ -1,4 +1,4 @@
 <div class="welcome">
   <h1>nested home load</h1>
-  <h2>Hello {$ns.demoDto->getName()}</h2>
+  <h2>Hello</h2>
 </div>
